07c9d72b9b9d698e0477647f065cfb057968a024
MrkielCode <ikechukwi.ezekiel@gmail.com>
2023-04-25 17:43:57 -0700

    add new prototype

--- main.h
+++ main.h
@@ -3,10 +3,10 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
-
+#include <stdio.h>
 int custom_putchar(char c);
 int custom_puts(char *str);
+Int print_integer(int a);
 int _printf(const char *format, ...);
-
 
 #endif /* MAIN_H */
