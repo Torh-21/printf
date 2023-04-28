03921c51132d62ee3602347b82f3dae5a29e61a9
Torh-21 <majarotoheeb@gmail.com>
2023-04-28 13:37:54 -0700

    New files added

--- main.h
+++ main.h
@@ -4,9 +4,18 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+typedef struct formats
+{
+	char *strp;
+	int (*func)(va_list);
+} check_format;
+
 int custom_putchar(char c);
 int custom_puts(char *str);
 int print_integer(int a);
+int print_binary(va_list input);
+int print_percent_sign(__attribute__((unused))va_list input);
+int format_checker(const char *format, va_list input);
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
---
+++ print_percent_sign.c
@@ -0,0 +1,13 @@
+#include “main.h”
+/**
+ * print_percent_sign - prints the percent symbol
+ *
+ * @input: input argument
+ *
+ * Return: integer
+ */
+int print_percent_sign(__attribute__((unused))va_list input)
+{
+	custom_putchar('%');
+	return (-1);
+}
